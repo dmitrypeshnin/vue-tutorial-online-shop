28013b0fc4ea2bc3cd7bb661f84a344689fa8e8f
dmitrypeshnin <nexa0984@gmail.com>
2022-11-25 08:50:52 +0700

    Completed display products cards with images

--- src/components/v-catalog-item.vue
+++ src/components/v-catalog-item.vue
@@ -1,27 +1,38 @@
 <template>
-    <div class="v-catalog-item">
-        <img src="image.jpg" alt="image">
-        <p>Item 1</p>
-        <p>Price: 100</p>
-        <button>Add to cart</button>
-    </div>
+  <div class="v-catalog-item">
+    <img class="v-catalog-item__image" :src=" require('../assets/images/' + product_data.image) " alt="">
+    <p class="v-catalog-item__name">{{ product_data.name }}</p>
+    <p class="v-catalog-item__prive">Price: ${{ product_data.price }}</p>
+    <button class="v-catalog-item__add-to-cart-btn btn">Add to cart</button>
+  </div>
 </template>
 
 <script>
-   export default {
-    name: 'v-catalog-item',
-    props: {},
-    data() {
-        return {}
+export default {
+  name: "v-catalog-item",
+  props: {
+    product_data: {
+      type: Object,
+      default() {
+        return {};
+      },
     },
-    computed: {}
-   }
+  },
+  data() {
+    return {};
+  },
+  computed: {},
+};
 </script>
 
 <style lang="scss">
-    .v-catalog-item {
-        flex-basis: 25%;
-        box-shadow: 0 0 8px 0 #e0e0e0;
-        padding: $padding * 2;
-    }
+.v-catalog-item {
+  flex-basis: 25%;
+  box-shadow: 0 0 8px 0 #e0e0e0;
+  padding: 16px;
+  margin-bottom: 16px;
+  &__image {
+    width: 100px;
+  }
+}
 </style>
--- src/components/v-catalog.vue
+++ src/components/v-catalog.vue
@@ -2,7 +2,11 @@
     <div class="v-catalog">
         <h1>Catalog</h1>
         <div class="v-catalog__list">
-            <v-catalog-item />
+            <v-catalog-item
+                v-for="product in products"
+                v-bind:key="product.article"
+                v-bind:product_data="product"
+             />
         </div>
     </div>
 </template>
@@ -19,7 +23,58 @@ import vCatalogItem from './v-catalog-item.vue';
         },
         props: {},
         data() {
-            return {}
+            return {
+                "products": [
+                    {
+                        image: "1.jpg",
+                        name: "T-shirt 1",
+                        price: 2100,
+                        article: "T1",
+                        available: true,
+                        category: "Мужские"
+                    },
+                    {
+                        image: "2.jpg",
+                        name: "T-shirt 2",
+                        price: 3150,
+                        article: "T2",
+                        available: true,
+                        category: "Женские"
+                    },
+                    {
+                        image: "3.jpg",
+                        name: "T-shirt 3",
+                        price: 4200,
+                        article: "T3",
+                        available: false,
+                        category: "Женские"
+                    },
+                    {
+                        image: "4.jpg",
+                        name: "T-shirt 4",
+                        price: 5300,
+                        article: "T4",
+                        available: true,
+                        category: "Мужские"
+                    },
+                    {
+                        image: "5.jpg",
+                        name: "T-shirt 5",
+                        price: 6500,
+                        article: "T5",
+                        available: false,
+                        category: "Женские"
+                    },
+                    {
+                        image: "6.jpeg",
+                        name: "T-shirt 6",
+                        price: 8700,
+                        article: "T6",
+                        available: true,
+                        category: "Женские"
+                    }
+  ]
+            }
         },
         computed: {
 
